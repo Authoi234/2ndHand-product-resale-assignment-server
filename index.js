2ad52af29a2bf92bf473392406d4dae8d1ec9dd0
Authoi234 <jawadkhan.authoi@gmail.com>
2024-02-26 16:32:11 +0600

    simple skeleton is complete

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// Port
+const port = process.env.PORT || 5000;
+
+// App
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6iupoas.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    const categoriesCollection = client.db('authois-product-resale').collection('categories');
+    
+  } finally {
+
+  }
+}
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Authois 2nd hand Product relase server')
+});
+
+app.listen(port, () => {
+    console.log(`Authois 2nd hand product resale server is running on port: ${port}`)
+})
